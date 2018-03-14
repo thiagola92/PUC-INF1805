2541e6c0f460da7b93a73221d904166845da025a
thiagola92 <thiagola92@gmail.com>
2018-03-14 13:05:19 -0300

    Tarefa-02

--- Aula-02/Tarefa-02/event_driven/app.ino
+++ Aula-02/Tarefa-02/event_driven/app.ino
@@ -2,19 +2,70 @@
 #include "app.h"
 #include "pindefs.h"
 
+double blink_timer = 1000;
+int led_state = 1;
+
+long button1_time_pressed = -1;
+long button2_time_pressed = -1;
+
 void appint(void)
 {
   button_listen(KEY1);
   button_listen(KEY2);
   button_listen(KEY3);
+
+  pinMode(LED4, OUTPUT);
+
+  timer_set(blink_timer);
 }
 
 void button_changed(int p, int v)
 {
-  
+  Serial.println("button_changed");
+  if(p == KEY1 && v == PRESSED_DOWN)
+  {
+    button1_time_pressed = millis();
+    blink_timer /= 2;
+  }
+  else if (p == KEY2 && v == PRESSED_DOWN)
+  {
+    button2_time_pressed = millis();
+    blink_timer *= 2;
+  }
+
+  if(buttons_pressed())
+    while(1);
+}
+
+boolean buttons_pressed()
+{
+  if(buttons_pressed_before())
+    return pressed_interval_inside(500);
+
+  return false;
+}
+
+boolean buttons_pressed_before()
+{
+  if(button1_time_pressed != -1 && button2_time_pressed != -1)
+    return true;
+  return false;
+}
+
+boolean pressed_interval_inside(long max_interval)
+{
+  long interval = abs(button1_time_pressed - button2_time_pressed);
+  Serial.println(interval);
+
+  if(interval < max_interval)
+    return true;
+  return false;
 }
 
 void timer_expired(void)
 {
-  
+  Serial.println("timer_expired");
+  led_state = !led_state;
+  digitalWrite(LED4, led_state);
+  timer_set(blink_timer);
 }
--- Aula-02/Tarefa-02/event_driven/event_driven.ino
+++ Aula-02/Tarefa-02/event_driven/event_driven.ino
@@ -2,19 +2,22 @@
 #include "app.h"
 #include "pindefs.h"
 
-#define BUTTONS_SIZE 3
+#define PRESSED_DOWN 0
+#define BUTTONS 3
 
-static unsigned long time_waited;
-static int buttons_listening[BUTTONS_SIZE];
+unsigned long time_waited;
+int buttons_listening[BUTTONS];
+boolean is_button_pressed[BUTTONS];
 
 void button_listen(int pin)
 {
-  for(int i = 0; i < BUTTONS_SIZE; ++i)
+  for(int i = 0; i < BUTTONS; ++i)
   {
-    if(buttons_listening[i] == 0)
+    if(buttons_listening[i] == -1)
     {
       buttons_listening[i] = pin;
       pinMode(pin, INPUT_PULLUP);
+      return;
     }
   }
 }
@@ -29,8 +32,7 @@ void setup()
   Serial.begin(9600);
   
   time_waited = 0;
-  for(int i = 0; i < BUTTONS_SIZE; ++i)
-    buttons_listening[i] = -1;
+  initialize_buttons();
   
   appint();
 }
@@ -38,29 +40,54 @@ void setup()
 void loop()
 {
   check_buttons();
-  
-  check_time();
+  check_timer();
+}
+
+void initialize_buttons()
+{
+  for(int i = 0; i < BUTTONS; ++i)
+  {
+    buttons_listening[i] = -1;
+    is_button_pressed[i] = false;
+  }
 }
 
 void check_buttons()
 {
-  for(int i = 0; i < BUTTONS_SIZE; ++i)
+  for(int i = 0; i < BUTTONS; ++i)
     if(buttons_listening[i] != -1)
-      check_button(buttons_listening[i]);
+      check_button(i);
 }
 
-void check_button(int button)
+void check_button(int button_id)
 {
+  int button = buttons_listening[button_id];
   int button_val = digitalRead(button);
-  if(button_val)
+  
+  if(is_button_changed(button_id, button_val) == true)
     button_changed(button, button_val);
 }
 
-void check_time()
+boolean is_button_changed(int button_id, int button_val)
+{
+  if(button_val == PRESSED_DOWN && is_button_pressed[button_id] == false ||
+    button_val == !PRESSED_DOWN && is_button_pressed[button_id] == true)
+  {
+    is_button_pressed[button_id] = !is_button_pressed[button_id];
+    return true;
+  }
+  
+  return false;
+}
+
+void check_timer()
 {
   if(time_waited != 0)
   {
     if(millis() >= time_waited)
+    {
+      time_waited = 0;
       timer_expired();
+    }
   }
 }
