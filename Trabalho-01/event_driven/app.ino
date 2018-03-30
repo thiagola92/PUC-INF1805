a46951dcb8d99dd029f986b30f1337a6ff003168
thiagola92 <thiagola92@gmail.com>
2018-03-30 18:08:02 -0300

    Exemplo do schedule por enquanto

--- Trabalho-01/event_driven/app.ino
+++ Trabalho-01/event_driven/app.ino
@@ -10,6 +10,16 @@ void appint()
 {
   buzzerSetup();
   displaySetup();
+  
+  addSchedule(0, 1000);
+  addSchedule(1, 100);
+  addSchedule(2, 2000);
+  addSchedule(3, 100);
+  addSchedule(4, 3000);
+  addSchedule(5, 100);
+
+  timer_set(1);
+  Serial.println("appint");
 }
 
 void button_changed(int p, int v)
@@ -19,6 +29,8 @@ void button_changed(int p, int v)
 
 void timer_expired()
 {
-  
+  Serial.println("timer_expired");
+  playSchedule();
+  timer_set(0);
 }
 
