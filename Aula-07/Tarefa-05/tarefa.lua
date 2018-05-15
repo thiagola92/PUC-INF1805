8b72eeeaa2a1e255fdaba885f02cdd687d43e890
thiagola92 <thiagola92@gmail.com>
2018-05-14 21:30:48 -0300

    Tarefa das leds piscando

--- Aula-07/Tarefa-05/tarefa.lua
+++ Aula-07/Tarefa-05/tarefa.lua
@@ -6,6 +6,8 @@ pin_button_2 = 2
 
 interval_speed = 1000
 
+stop_leds = false
+
 timer = tmr.create()
 
 gpio.mode(pin_led_1, gpio.OUTPUT)
@@ -17,10 +19,15 @@ gpio.mode(pin_button_2, gpio.INT, gpio.PULLUP)
 gpio.write(pin_led_1, gpio.HIGH)
 gpio.write(pin_led_2, gpio.LOW)
 
-function change_led_speed(signal)
+last_button_pressed = 0
+last_time_button_pressed = 0
+
+function change_led_speed(signal, button)
   local last_call = 0
 
-  local function is_on_cooldown(now)
+  local function is_on_cooldown()
+    local now = tmr.now()
+
     if(now - last_call > 50000) then
       return false
     end
@@ -28,8 +35,35 @@ function change_led_speed(signal)
     return true
   end
 
+  local function should_stop_leds()
+    local now = tmr.now()
+
+    if(button ~= last_button_pressed and
+        now - last_time_button_pressed < 1000000) then
+      stop_leds = true
+    else
+      last_button_pressed = button
+      last_time_button_pressed = tmr.now()
+    end
+
+    return stop_leds
+  end
+
+  local function should_run_callback()
+    if(stop_leds == true) then
+      return false
+    end
+
+    if(should_stop_leds() == true or
+        is_on_cooldown() == true) then
+      return false
+    end
+
+    return true
+  end
+
   local function callback(level, when, eventcount)
-    if(is_on_cooldown(tmr.now())) then
+    if(should_run_callback() == false) then
       return
     end
 
@@ -46,6 +80,10 @@ function change_led_speed(signal)
 end
 
 function change_leds_states()
+  if(stop_leds == true) then
+    return
+  end
+
   if(gpio.read(pin_led_1) == gpio.LOW) then
     gpio.write(pin_led_1, gpio.HIGH)
   else
@@ -61,10 +99,10 @@ function change_leds_states()
   print("led state 1: ", gpio.read(pin_led_1))
   print("led state 2: ", gpio.read(pin_led_2))
 
-  timer:alarm(interval_speed, tmr.ALARM_SINGLE, change_leds_states())
+  timer:alarm(interval_speed, tmr.ALARM_SINGLE, change_leds_states)
 end
 
-gpio.trig(pin_button_1, "down", change_led_speed(-1))
-gpio.trig(pin_button_2, "down", change_led_speed(1))
+gpio.trig(pin_button_1, "down", change_led_speed(-1, pin_button_1))
+gpio.trig(pin_button_2, "down", change_led_speed(1, pin_button_2))
 
-timer:alarm(interval_speed, tmr.ALARM_SINGLE, change_leds_states())
+timer:alarm(interval_speed, tmr.ALARM_SINGLE, change_leds_states)
