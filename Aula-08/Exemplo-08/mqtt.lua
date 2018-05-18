26f7630a45cb02fc22805beddb88b82bdf8b3889
thiagola92 <thiagola92@gmail.com>
2018-05-18 16:43:45 -0300

    Short examples, still have to comment exemplo-08

--- Aula-08/Exemplo-08/mqtt.lua
+++ Aula-08/Exemplo-08/mqtt.lua
@@ -2,6 +2,10 @@
 
 local client = mqtt.Client("thiagola92", 120)
 
+-- Everytime the client receive one message, it will call this function
+-- The function receive the client that receive the message
+-- The topic from where is the message
+-- The message
 function message_callback(client, topic, message)
   print("message_callback(" .. topic .. ", " .. message .. ")")
 end
@@ -9,6 +13,14 @@ end
 function subscribe_callback(client)
   print("subscribe_callback")
 
+  -- Set a callback function for an event in this client
+  -- First argument is the name of the event, it can be message/connect/offline
+  -- You can read the function as "on message"/"on connect"/"on offline"
+  -- The second argument is the function to be called when the event happens.
+  -- If your event is connect/offline, the function will receive 1 argument
+  -- client
+  -- Else if your event is message, the function will receive 3 arguments
+  -- client, topic, message
   client:on("message", message_callback)
 end
 
