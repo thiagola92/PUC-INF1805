033404e8df79a3affa18089d803a6859874b1b26
thiagola92 <thiagola92@gmail.com>
2018-06-30 03:50:07 -0300

    Adicionando o ajuste de resolução que o leo trouxe

---
+++ Trabalho-04/client/configuration.lua
@@ -0,0 +1,12 @@
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+
+SCREEN_WIDTH = love.graphics.getWidth()
+SCREEN_HEIGHT = love.graphics.getHeight()
+
+love.window.setMode(GAME_WIDTH, GAME_HEIGHT, {resizable = true})
+
+-----------------------------------------------------------------------------
+
+function draw()
+end
--- Trabalho-04/client/main.lua
+++ Trabalho-04/client/main.lua
@@ -1,11 +1,20 @@
 local new_client = require("client")
+local resolution = require("res")
+require("configuration")
 
 function love.load(args)
-  client = new_client()
+  client = new_client(args[2], args[3])
+  resolution.set("fit", GAME_WIDTH, GAME_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT)
+end
+
+function love.resize(w, h)
+  SCREEN_WIDTH = w
+  SCREEN_HEIGHT = h
+  resolution.set("fit", GAME_WIDTH, GAME_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT)
 end
 
 function love.draw()
-
+  resolution.render(draw)
 end
 
 function love.update(dt)
